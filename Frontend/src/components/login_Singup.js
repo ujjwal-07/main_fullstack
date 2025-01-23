339e9d40b33f4e62e867cd7a8cbc02d1f80473b3
ujjwal-07 <68552317+ujjwal-07@users.noreply.github.com>
2025-01-23 23:32:10 +0530

    Update login_Singup.js

--- Frontend/src/components/login_Singup.js
+++ Frontend/src/components/login_Singup.js
@@ -26,7 +26,7 @@ const AuthPage = () => {
 
     if (isLogin) {
         payload = { email, password };
-        apiUrl = 'https://up-social-backend.onrender.com/user/login';
+        apiUrl = 'http://localhost:3002/user/login';
         try {
           const response = await axios.post(apiUrl, payload);
   
@@ -42,7 +42,7 @@ const AuthPage = () => {
       }
     } else {
         payload = { fname, lname, email, password };
-        apiUrl = 'https://up-social-backend.onrender.com/user/adduser';
+        apiUrl = 'http://localhost:3002/user/adduser';
         try {
           const response = await axios.post(apiUrl, payload);
   
