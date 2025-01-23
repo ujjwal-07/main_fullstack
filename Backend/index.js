b4cde3863c87cead170a8ee215bf431c3819b5c5
ujjwal-07 <68552317+ujjwal-07@users.noreply.github.com>
2025-01-23 22:17:17 +0530

    added vercel.json

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,30 @@
+// api/index.js
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors")
-const userRouter = require("./routes/userRouter");
-const emailRouter = require("./routes/emailRoutes");
-const postRouter = require("./routes/postRoutes");
-const bodyParser = require('body-parser')
+const cors = require("cors");
+const userRouter = require("../routes/userRouter");
+const emailRouter = require("../routes/emailRoutes");
+const postRouter = require("../routes/postRoutes");
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use("/user",userRouter)
-app.use("/email",emailRouter)
-app.use("/posts",postRouter)
+// Routers
+app.use("/user", userRouter);
+app.use("/email", emailRouter);
+app.use("/posts", postRouter);
 
+// MongoDB connection
 mongoose.connect("mongodb+srv://employedatsstore:Ujjwal21@cluster0.i81xi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(()=>{console.log("Connected to Mongo Atlas")})
-.catch((err)=>{console.log("Error occured : ",err)})
+  .then(() => {
+    console.log("Connected to Mongo Atlas");
+  })
+  .catch((err) => {
+    console.log("Error occurred : ", err);
+  });
 
-
-app.listen(3002,()=>{
-    console.log("App is running on port 3002")
-})
-
-
-// using smtp server
-
+// Export Express app as a handler for serverless function
+module.exports = app;
