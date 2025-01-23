fa6cb482059fb7cb109eae13cda90a664810b5d8
ujjwal-07 <68552317+ujjwal-07@users.noreply.github.com>
2025-01-24 00:32:02 +0530

    final commitr this can be

--- Backend/index.js
+++ Backend/index.js
@@ -18,6 +18,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json())
 
+
+mongoose.connect(process.env.DB)
+.then(()=>{console.log("Connected to Mongo Atlas")})
+.catch((err)=>{console.log("Error occured : ",err)})
+
 app.use("/user",userRouter)
 app.use("/email",emailRouter)
 app.use("/posts",postRouter)
@@ -25,36 +30,7 @@ app.get("/",(req,res)=>{
     res.send("<h1>Hello</h1>")
 })
 
-app.post("/user/login",async (req,res)=>{
-    try{
-        const {email, password} = req.body;
-        const checkUser = await User.findOne({email:email})
-        console.log(checkUser)
-        if(!checkUser){
-            res.status(400).send("Invalid Email")
-            return
-        }
 
-        const passwordCheck = await bcrypt.compare(password, checkUser.password)
-        if(!passwordCheck){
-            console.log("Incorrect Password")
-            return res.status(400).send("Invalid credentials")
-            
-
-        }
-        
-        return  res.status(200).json({ token: generateToken(checkUser.email), email:{email} });
-        
-
-    }catch(error){
-        console.error("Error fetching users:", error);
-        return  res.status(500)
-        
-    }
-})
- mongoose.connect(process.env.DB)
-.then(()=>{console.log("Connected to Mongo Atlas")})
-.catch((err)=>{console.log("Error occured : ",err)})
 
 
 app.listen(3002,()=>{
