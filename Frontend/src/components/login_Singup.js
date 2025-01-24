93312d964611548cea5f6452b160bc3d444f1011
ujjwal-07 <68552317+ujjwal-07@users.noreply.github.com>
2025-01-24 22:01:14 +0530

    commit

--- Frontend/src/components/login_Singup.js
+++ Frontend/src/components/login_Singup.js
@@ -2,85 +2,68 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-
-
 const AuthPage = () => {
   const navigate = useNavigate();
-  // State variables for form fields
-  const [isLogin, setIsLogin] = useState(true); // Track if it's the login or signup form
+  const [isLogin, setIsLogin] = useState(true); // Track login/signup state
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [fname, setFname] = useState('');
   const [lname, setLname] = useState('');
+  const [loading, setLoading] = useState(false); // Loader state
 
   // Toggle between login and signup form
   const toggleForm = () => {
     setIsLogin(!isLogin);
   };
 
+  // Handle form submission (login/signup)
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     let payload = {};
     let apiUrl = '';
 
+    setLoading(true); // Start the loader
+
     if (isLogin) {
-        payload = { email, password };
-        apiUrl = 'https://up-social-backend.onrender.com/user/login';
-        try {
-          const response = await axios.post(apiUrl, payload);
-  
-          // Save the token in localStorage
-          const token = response.data.token;
-        
-          localStorage.setItem('authToken', token);
-          localStorage.setItem('email', response.data.email.email)
-          console.log('Success:', response.data);
-          navigate('/'); // Navigate to the home page
+      payload = { email, password };
+      apiUrl = 'https://up-social-backend.onrender.com/user/login';
+      try {
+        const response = await axios.post(apiUrl, payload);
+        const token = response.data.token;
+        localStorage.setItem('authToken', token);
+        localStorage.setItem('email', response.data.email.email);
+        console.log('Success:', response.data);
+        navigate('/');
       } catch (error) {
-          alert(error.response.data)
-          console.error('Error:', error.response ? error.response.data : error.message);
+        alert(error.response.data);
+        console.error(error.response ? error.response.data : error.message);
+      } finally {
+        setLoading(false); // Stop loader
       }
     } else {
-        payload = { fname, lname, email, password };
-        apiUrl = 'https://up-social-backend.onrender.com/user/adduser';
-        try {
-          const response = await axios.post(apiUrl, payload);
-  
-          // Save the token in localStorage
-          const token = response.data.token;
-        
-          localStorage.setItem('authToken', token);
-          localStorage.setItem('email', response.data.email)
-          console.log('Success:', response.data);
-          navigate('/'); // Navigate to the home page
+      payload = { fname, lname, email, password };
+      apiUrl = 'https://up-social-backend.onrender.com/user/adduser';
+      try {
+        const response = await axios.post(apiUrl, payload);
+        const token = response.data.token;
+        localStorage.setItem('authToken', token);
+        localStorage.setItem('email', response.data.email);
+        console.log('Success:', response.data);
+        navigate('/');
       } catch (error) {
-        alert(error.response.data)
-          console.error('Error:', error.response ? error.response.data : error.message);
-          setEmail('')
-          setFname('')
-          setLname('')
-          setPassword('')
-          navigate("/login")
+        alert(error.response.data);
+        console.error(error.response ? error.response.data : error.message);
+        setEmail('');
+        setFname('');
+        setLname('');
+        setPassword('');
+        navigate("/login");
+      } finally {
+        setLoading(false); // Stop loader
       }
     }
-
-    // try {
-    //     const response = await axios.post(apiUrl, payload);
-
-    //     // Save the token in localStorage
-    //     const token = response.data.token;
-      
-    //     localStorage.setItem('authToken', token);
-    //     localStorage.setItem('email', response.data.email.email)
-    //     console.log('Success:', response.data);
-    //     navigate('/'); // Navigate to the home page
-    // } catch (error) {
-    //     console.error('Error:', error.response ? error.response.data : error.message);
-    // }
-};
-
-
+  };
 
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
@@ -88,71 +71,78 @@ const AuthPage = () => {
         <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
           {isLogin ? 'Login' : 'Sign Up'}
         </h2>
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          {!isLogin && (
-            <>
+
+        <div className="flex justify-center items-center">
+          {loading ? (
+            <div className="w-16 h-16 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+          ) : (
+            <form className="space-y-6" onSubmit={handleSubmit}>
+              {!isLogin && (
+                <>
+                  <div>
+                    <label htmlFor="fname" className="block text-gray-700">First Name</label>
+                    <input
+                      type="text"
+                      id="fname"
+                      placeholder="Enter your first name"
+                      className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                      value={fname}
+                      onChange={(e) => setFname(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="lname" className="block text-gray-700">Last Name</label>
+                    <input
+                      type="text"
+                      id="lname"
+                      placeholder="Enter your last name"
+                      className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                      value={lname}
+                      onChange={(e) => setLname(e.target.value)}
+                      required
+                    />
+                  </div>
+                </>
+              )}
+
               <div>
-                <label htmlFor="fname" className="block text-gray-700">First Name</label>
+                <label htmlFor="email" className="block text-gray-700">Email</label>
                 <input
-                  type="text"
-                  id="fname"
-                  placeholder="Enter your first name"
+                  type="email"
+                  id="email"
+                  placeholder="Enter your email"
                   className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  value={fname}
-                  onChange={(e) => setFname(e.target.value)}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                 />
               </div>
+              
               <div>
-                <label htmlFor="lname" className="block text-gray-700">Last Name</label>
+                <label htmlFor="password" className="block text-gray-700">Password</label>
                 <input
-                  type="text"
-                  id="lname"
-                  placeholder="Enter your last name"
+                  type="password"
+                  id="password"
+                  placeholder="Enter your password"
                   className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  value={lname}
-                  onChange={(e) => setLname(e.target.value)}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                 />
               </div>
-            </>
+
+              <div className="flex justify-between items-center">
+                <button
+                  type="submit"
+                  className="w-full py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none"
+                >
+                  {isLogin ? 'Login' : 'Sign Up'}
+                </button>
+              </div>
+            </form>
           )}
-
-          <div>
-            <label htmlFor="email" className="block text-gray-700">Email</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          
-          <div>
-            <label htmlFor="password" className="block text-gray-700">Password</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-
-          <div className="flex justify-between items-center">
-            <button
-              type="submit"
-              className="w-full py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none"
-            >
-              {isLogin ? 'Login' : 'Sign Up'}
-            </button>
-          </div>
-        </form>
+        </div>
 
         <div className="mt-4 text-center">
           <span className="text-gray-600">
